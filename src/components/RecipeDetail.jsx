e3cf414fb745bf15048ce65bea9c891fd3d64b15
kpomm <katherine.l.pommerening.25@dartmouth.edu>
2023-10-25 13:49:43 -0400

    Minor styling changes

--- src/components/RecipeDetail.jsx
+++ src/components/RecipeDetail.jsx
@@ -44,35 +44,37 @@ function RecipeDetail({ recipe, marketsPromise }) {
 
   return (
     <div id="recipe-detail" className="fixed-element">
-      <div className="detail-header"><h5 id="recipe-title">{recipe.title}</h5><img src={recipe.image} alt="" /></div>
-      <div id="detail-container">
-        <div className="details">
-          <h3>Quick Facts</h3>
+      <div className="columns">
+        <div className="col-2">
           <div>
-            <h4>Ready in {recipe.readyInMinutes} minutes
-            </h4>
+            <h5 id="recipe-title">{recipe.title}</h5>
+            { recipe.dishTypes && (<div id="dish-types">{dishTypes}</div>)}
           </div>
-          <div>
-            <h4>Is is affordable? {recipe.cheap}
-            </h4>
-          </div>
-          <div>
-            <h4>Servings: {recipe.servings}
-            </h4>
-          </div>
-          { recipe.dishTypes && (<div><h4>Dish Types: <div id="dish-types">{dishTypes}</div></h4></div>)}
-          <div>
-            <h4>Source: {' '}
-              <a href={recipe.sourceUrl}>{recipe.sourceName}</a>
-            </h4>
-          </div>
+          <img src={recipe.image} alt="" />
         </div>
-        <div className="details-right">
+        <div className="col-2">
+          <div>
+            <div>
+              <h4>Ready in {recipe.readyInMinutes} minutes
+              </h4>
+            </div>
+            <div>
+              <h4>Servings: {recipe.servings}
+              </h4>
+            </div>
+            <div>
+              <h4>Source: {' '}
+                <a href={recipe.sourceUrl}>{recipe.sourceName}</a>
+              </h4>
+            </div>
+          </div>
           {markets && (<div><h3>Shop Local!</h3><div className="markets">{displayMarkets}</div></div>)}
-        </div>
-      </div>
 
+        </div>
+
+      </div>
     </div>
+
   );
 }
 
