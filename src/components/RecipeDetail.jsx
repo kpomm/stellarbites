1050d03f0ef5969f49bc1b18a66811b8afb5f523
kpomm <katherine.l.pommerening.25@dartmouth.edu>
2023-10-24 23:01:11 -0400

    some more last touches

--- src/components/RecipeDetail.jsx
+++ src/components/RecipeDetail.jsx
@@ -37,7 +37,7 @@ function RecipeDetail({ recipe, marketsPromise }) {
   if (markets) {
     displayMarkets = markets.map((mkt) => {
       return (
-        <h4 className="dish-type">{mkt}</h4>
+        <h4 className="farmers-market">{mkt.market_name}</h4>
       );
     });
   }
@@ -45,30 +45,33 @@ function RecipeDetail({ recipe, marketsPromise }) {
   return (
     <div id="recipe-detail" className="fixed-element">
       <div className="detail-header"><h5 id="recipe-title">{recipe.title}</h5><img src={recipe.image} alt="" /></div>
-      <div className="details">
-        <h3>Quick Facts</h3>
-        <div>
-          <h4>Ready in {recipe.readyInMinutes} minutes
-          </h4>
+      <div id="detail-container">
+        <div className="details">
+          <h3>Quick Facts</h3>
+          <div>
+            <h4>Ready in {recipe.readyInMinutes} minutes
+            </h4>
+          </div>
+          <div>
+            <h4>Is is affordable? {recipe.cheap}
+            </h4>
+          </div>
+          <div>
+            <h4>Servings: {recipe.servings}
+            </h4>
+          </div>
+          { recipe.dishTypes && (<div><h4>Dish Types: <div id="dish-types">{dishTypes}</div></h4></div>)}
+          <div>
+            <h4>Source: {' '}
+              <a href={recipe.sourceUrl}>{recipe.sourceName}</a>
+            </h4>
+          </div>
         </div>
-        <div>
-          <h4>Is is affordable? {recipe.cheap}
-          </h4>
-        </div>
-        <div>
-          <h4>Servings: {recipe.servings}
-          </h4>
-        </div>
-        { recipe.dishTypes && (<div><h4>Dish Types: <div id="dish-types">{dishTypes}</div></h4></div>)}
-        <div>
-          <h4>Source: {' '}
-            <a href={recipe.sourceUrl}>{recipe.sourceName}</a>
-          </h4>
+        <div className="details-right">
+          {markets && (<div><h4>Shop Local!</h4><div className="markets">{displayMarkets}</div></div>)}
         </div>
       </div>
-      <div className="details-right">
-        {markets && (<div><h4>Shop Local!</h4><div className="markets">{displayMarkets}</div></div>)}
-      </div>
+
     </div>
   );
 }
